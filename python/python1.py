51bf2eedfb4834829279c9288c80bb2fdd6d05fc
erlinares <erlinares@gmail.com>
2020-05-22 20:36:53 -0500

    Add files via upload

---
+++ python/python1.py
@@ -0,0 +1,61 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Value x:  2\n",
+      "Value y:  4\n",
+      "Value x*y:  8\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Create 3 variables, x=2, y=4 and z=x*y \n",
+    "# x=2 assign the value 2 in the variable x\n",
+    "# y=4 assign the value 4 in the variable y\n",
+    "# z=x*y assign the value 2*4 in the variable z\n",
+    "x=2;  \n",
+    "y=4;\n",
+    "z=x*y;\n",
+    "# Print text \"Value...\" and the value of the variable\n",
+    "print (\"Value x: \", x);\n",
+    "print (\"Value y: \", y);\n",
+    "print (\"Value x*y: \", z);"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
